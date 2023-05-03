2b2ce5c4d6b582de53bf619b8748026cd0b16ec4
tonialtendorf <tvohnoutka05@gmail.com>
2023-05-03 11:47:45 -0500

    updated files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/altendorf-portfolio">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
